d624e68c199e9539a16e93713268ff25e1ba706a
Spancheg <spanchege@yandex.ru>
2025-07-20 16:19:40 +0300

    Изменил формулу

--- dz.4.12.js
+++ dz.4.12.js
@@ -3,7 +3,7 @@ const addressLong = 10;
 const positionLat = 20;
 const positionLong = 20;
 
-const res = (positionLong - addressLong)**2 + (positionLat - addressLat)**2;
+const res = Math.acos(Math.sin(addressLat)*Math.sin(positionLat) + Math.cos(addressLat)*Math.cos(positionLat)*Math.cos(positionLong - addressLong))
 
 console.log('Расстояние равно ' + res);
 
