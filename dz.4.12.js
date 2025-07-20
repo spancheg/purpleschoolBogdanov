304abef2054b314b1e821cd943668c26bef28173
Spancheg <spanchege@yandex.ru>
2025-07-20 16:25:13 +0300

    добавил радиус

--- dz.4.12.js
+++ dz.4.12.js
@@ -2,8 +2,9 @@ const addressLat = 10;
 const addressLong = 10;
 const positionLat = 20;
 const positionLong = 20;
+const R = 6371
 
-const res = Math.acos(Math.sin(addressLat)*Math.sin(positionLat) + Math.cos(addressLat)*Math.cos(positionLat)*Math.cos(positionLong - addressLong))
+const res = R*Math.acos(Math.sin(addressLat)*Math.sin(positionLat) + Math.cos(addressLat)*Math.cos(positionLat)*Math.cos(positionLong - addressLong))
 
 console.log('Расстояние равно ' + res);
 
